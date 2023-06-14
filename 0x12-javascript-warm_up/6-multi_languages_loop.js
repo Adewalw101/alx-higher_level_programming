1b96a377d2a243e5e7ea429520c5f8c2da0bea4a
Adewalw101 <elijah2ade@yahoo.co.uk>
2023-06-14 14:47:26 -0700

    testing Java skill

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0, len = myArray.length; i < len; i++) {
+  console.log(myArray[i]);
+}
+Footer
+© 2023 GitHub, Inc
